a8109e83a333230af8a0b6a8876b4827838372db
Dave Kilian <dave.kilian@gmail.com>
2012-10-12 22:39:06 -0400

    Definitions for data class objects

--- rapi.py
+++ rapi.py
@@ -10,9 +10,7 @@ import xml.etree.ElementTree as ET
 _login_url = "https://playback.rhapsody.com/login.xml"
 
 class Session:
-    """
-    Caches information about the user currently logged in
-    """
+    """ Caches information about the user currently logged in """
 
     cobrandId = ""
     token = ""
@@ -22,6 +20,62 @@ class Session:
         self.cobrandId = cobrandId
         self.token = token
         self.userGuid = userGuid
+
+class Artist:
+    """
+    Artist metadata
+
+    Attributes
+        id [str]        The artist's unique ID
+        name [str]      The human-readable name of this artist
+        image [str]     A URL to an image of this artist
+        albumids [list] A list of IDs [str] of albums by this artist
+    """
+
+    id = ""
+    name = ""
+    image = ""
+    albums = []
+
+class Album:
+    """
+    Album metadta
+
+    Attributes
+        id [str]        The album's unique ID
+        artistid [str]  The ID of the album artist
+        name [str]      The human-readable name of this album
+        art [str]       A URL To an image containing the album's album art
+        year [int]      The year this album was released
+        genre [str]     The genre the album is filed under
+        trackids [list] A list of IDs [str] of the tracks in this artist
+    """
+
+    id = ""
+    naem = ""
+    art = ""
+    year = 2000
+    genre = ""
+    tracks = []
+
+class Track:
+    """
+    Track metadata
+
+    Attributes
+        id[str]         The track's unique ID
+        artistid [str]  The ID of the track's album's album artist
+        albumid [str]   The ID of the album the track appears on
+        name [str]      The title of the track
+        duration [int]  The length of the track in seconds
+    """
+
+    id = ""
+    artistid = ""
+    albumid = ""
+    name = ""
+    duration = 0
+
 
 def log_in(username, password):
     """
